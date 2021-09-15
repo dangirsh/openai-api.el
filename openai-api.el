46fa1b475b1363dbe4dd90b6b19df097aa455650
Dan Girshovich <dan@worldcoin.org>
2021-09-15 22:47:10 +0200

    add support for:
    - listing engines
    - basic validation of key and engine
    - completing based on region

--- openai-api.el
+++ openai-api.el
@@ -47,16 +47,66 @@
                                        (n . 1)))
 
 
+(defun openai-api--validate-secret-key ()
+  (unless openai-api-secret-key
+    (error
+     (concat "You must configure your OpenAI secret key with (setq openai-api-secret-key <key>).\n"
+             "See https://beta.openai.com/account/api-keys"))))
+
+(defun openai-api--validate-engine ()
+  (unless (member openai-api-engine (openai-api-get-engines))
+    (error
+     (format "Invalid engine %s. Must be one of: %s" openai-api-engine (openai-api-get-engines)))))
+
+(defun openai-api--request-header ()
+  `(("Content-Type" . "application/json")
+    ("Authorization" . ,(format "Bearer %s" openai-api-secret-key))))
+
+
+(defun openai-api--get-request (url)
+  "Submit a POST request to the OpenAI API with given URL and BODY-PARAMS."
+  (openai-api--validate-secret-key)
+  (let ((response-body))
+    (request
+      url
+      :type "GET"
+      :headers (openai-api--request-header)
+      :parser 'json-read
+      :sync t
+      :success (cl-function
+                (lambda (&key response &allow-other-keys)
+                  (setq response-body (request-response-data response)))))
+    response-body))
+
+(defun openai-api--get-engines-request ()
+  (openai-api--get-request (format "%s/engines" openai-api--base-url)))
+
+(defun openai-api--parse-engines-response (engines-response)
+  (let* ((engines-alist (alist-get 'data engines-response))
+         (extract-result #'(lambda (engine) (alist-get 'id engine)))
+         (engine-names (seq-map extract-result engines-alist)))
+    engine-names))
+
+
+(defvar openai-engines-cache nil)
+
+(defun openai-api-get-engines (&optional no-cache)
+  (if (and openai-engines-cache (not no-cache))
+      openai-engines-cache
+    (setq openai-engines-cache
+          (openai-api--parse-engines-response
+           (openai-api--get-engines-request)))))
+
+
 (defun openai-api--post-request (url body-params)
   "Submit a POST request to the OpenAI API with given URL and BODY-PARAMS."
-  (unless openai-api-secret-key
-    (error "You must configure your OpenAI secret key with (setq openai-api-secret-key <key>). See https://beta.openai.com/account/api-keys"))
+  (openai-api--validate-secret-key)
+  (openai-api--validate-engine)
   (let ((response-body))
     (request
       url
       :type "POST"
-      :headers `(("Content-Type" . "application/json")
-                 ("Authorization" . ,(format "Bearer %s" openai-api-secret-key)))
+      :headers (openai-api--request-header)
       :data (json-encode-alist body-params)
       :parser 'json-read
       :sync t
@@ -64,6 +114,7 @@
                 (lambda (&key response &allow-other-keys)
                   (setq response-body (request-response-data response)))))
     response-body))
+
 
 (defun openai-api--sanitize-completion-params ()
   "Fixup wasteful or meanless completion parameter settings, and return better ones."
@@ -96,6 +147,16 @@
   "Return a list of completions for PROMPT, with length given by the request body param 'n'."
   (openai-api--parse-completion-response
    (openai-api--completion-request prompt)))
+
+(defun openai-api-complete-region (&optional b e)
+  "Get a completion for the current region."
+  (interactive "r")
+  (let* ((prompt (buffer-substring-no-properties (region-beginning) (region-end)))
+         (completions (openai-api-get-completions prompt)))
+    (deactivate-mark)
+    (goto-char e)
+    (newline)
+    (insert (completing-read "Choose completion:" completions))))
 
 (provide 'openai-api)
 
